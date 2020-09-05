8e07e961eb01acdcf2b195c23deddb464a940766
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2020-09-05 22:17:03 +0700

    start project

---
+++ src/main/java/id/co/mhanifmuhsin/adminlte/SpringbootAdminlteApplication.java
@@ -0,0 +1,13 @@
+package id.co.mhanifmuhsin.adminlte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminlteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdminlteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/mhanifmuhsin/adminlte/SpringbootAdminlteApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.mhanifmuhsin.adminlte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminlteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
